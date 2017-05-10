755da95ff5ceb8a8ac376d221406c196f2dd9185
ThuF <jordan.p.pavlov@gmail.com>
2017-05-10 11:33:53 +0300

    Initial Commit

---
+++ mail_proxy/ScriptingServices/mail/proxy/service.js
@@ -0,0 +1,69 @@
+/* globals $ */
+/* eslint-env node, dirigible */
+
+var request = require('net/http/request');
+var response = require('net/http/response');
+var mail = require('service/mail');
+
+handleRequest(request, response);
+
+function handleRequest(httpRequest, httpResponse) {
+	try {
+		dispatchRequest(httpRequest, httpResponse);
+	} catch (e) {
+		console.error(e);
+		sendResponse(httpResponse, httpResponse.BAD_REQUEST, 'text/plain', e);
+	}
+}
+
+function dispatchRequest(httpRequest, httpResponse) {
+	response.setContentType('application/json; charset=UTF-8');
+	response.setCharacterEncoding('UTF-8');
+
+	switch (httpRequest.getMethod()) {
+		case 'POST':
+			handlePostRequest(httpRequest, httpResponse);
+			break;
+		default:
+			handleNotAllowedRequest(httpResponse);
+	}
+}
+
+function handlePostRequest(httpRequest, httpResponse) {
+	var sendMailRequest = getRequestBody(httpRequest);
+
+	var from = sendMailRequest.from;
+	var to = sendMailRequest.to;
+	var subject = sendMailRequest.subject;
+	var content = sendMailRequest.content;
+	var type = sendMailRequest.type;
+
+	try {
+		mail.send(from, to, subject, content, type);
+		sendResponse(httpResponse, httpResponse.ACCEPTED);
+	} catch (e) {
+		sendResponse(httpResponse, httpResponse.BAD_REQUEST, 'application/json', JSON.stringify({
+			'errorMessage': e
+		}));
+	}
+}
+
+function handleNotAllowedRequest(httpResponse) {
+	sendResponse(httpResponse, httpResponse.METHOD_NOT_ALLOWED);
+}
+
+function getRequestBody(httpRequest) {
+	try {
+		return JSON.parse(httpRequest.readInputText());
+	} catch (e) {
+		return null;
+	}
+}
+
+function sendResponse(response, status, contentType, content) {
+	response.setStatus(status);
+	response.setContentType(contentType);
+	response.println(content);
+	response.flush();
+	response.close();	
+}
